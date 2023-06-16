749cdbf1e16238f2c64a975bb09af101d39ed0fb
chrisIyer <hellohotbox420@gmail.com>
2023-06-16 11:44:34 -0400

    photo change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import TodoList from './components/TodoList/TodoList'
 import ListGroup from 'react-bootstrap/ListGroup'
-import shower from './showPic.jpeg'
+import shower from './newShow.png'
 
 
 export default function App(){
