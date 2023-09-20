31fd944df44b361acc462f88c0da5f94bea8a318
chrisIyer <hellohotbox420@gmail.com>
2023-09-20 10:18:21 -0400

    not sure why not indexing.

--- models/blahg.js
+++ models/blahg.js
@@ -1,3 +1,4 @@
+//const comment = require('./comment')
 const { Schema, model } = require('mongoose')
 
 const blahgSchema = new Schema({
@@ -6,8 +7,11 @@ const blahgSchema = new Schema({
      category: String, 
      text: String,
      image: String 
+     //   likes: { type: Number, default: 0 },
+ // comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+
 }, {
 timestamps: true
 })
 
-module.exports = model('NewBlahg', blahgSchema)
+module.exports = model('Blahg', blahgSchema)
--- models/todo.js
+++ models/todo.js
@@ -1,12 +1,18 @@
 const { model, Schema } = require('mongoose')
 
 const todoSchema = new Schema({
+    //const commentSchema = new Schema({
+        //author: {required: false, type: String}
     title: {required: true, type: String},
+    //comment: {required: false, type: String},
     completed: { required: true, type: String}
+    //likes: {type:Number, default:0}
 }, {
     timestamps: true
 })
-
+//})
 const Todo = model('Todo', todoSchema)
+//const Comment = model('Comment', commentSchema)
 
 module.exports = Todo;
+//modeule.exports = Comment
