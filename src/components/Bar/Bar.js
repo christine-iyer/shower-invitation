f7e3aa81c2a58a7d8bf1c189206122e7885cfb4a
chrisIyer <hellohotbox420@gmail.com>
2024-04-30 17:40:34 -0400

    go

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -8,7 +8,7 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function Bar(props) {
   return (
     <>
-    <Navbar  className="navbar-left" bg="dark" variant="light" width='100%'>
+    <Navbar  style={{backgroundColor: 'rgba(23,45,65,.7)'}}className="navbar-left" bg="dark" variant="light" width='100%'>
       <Container >
 
     <Nav >
