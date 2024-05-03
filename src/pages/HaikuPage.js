3be3ec5e0b1f6f32326d635030f88bd136ba3f57
chrisIyer <hellohotbox420@gmail.com>
2024-05-03 10:46:03 -0400

    fitting.

--- src/pages/HaikuPage.js
+++ src/pages/HaikuPage.js
@@ -134,37 +134,6 @@ function HaikuPage() {
     listHaikus()
   }, [foundHaikus])
 
-  // const getSentimentScore = async () => {
-  //   const url = 'https://text-analysis12.p.rapidapi.com/sentiment-analysis/api/v1.1';
-  //   const options = {
-  //     method: 'POST',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //       'X-RapidAPI-Key': '5e4d0eeb5bmsh1f0574004d6dfb6p160e9fjsnd9a3ae03ad63',
-  //       'X-RapidAPI-Host': 'text-analysis12.p.rapidapi.com'
-  //     },
-  //     body: {
-  //       language: 'english',
-  //       text: 'Steam dances above, dark elixir soothes the soul,morning\'s gift of life.'
-  //     }
-  //   };
-
-  //   try {
-  //     const response = await fetch(url, options);
-  //     const result = await response.text();
-  //     console.log(result);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-  // useEffect(() => {
-  //   getSentimentScore()
-  // }, [])
-
-
-
-  // if the title field is empty, create a title. The title will be derived an array that contains all the words in the lines. They get jumbled together and then the first two are chosen.
-  // const createTitle = async (title) => {}
 
 
   return (
