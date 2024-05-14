0c95c81828ea154105ea25bd91586b1660088ddf
chrisIyer <hellohotbox420@gmail.com>
2024-05-14 15:41:34 -0400

    it worked.

--- controllers/api/haikus.js
+++ controllers/api/haikus.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
-const twilio = require('twilio')
 const Haiku = require('../../models/haiku')
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken);
+
 
 const deleteHaiku = async (req, res, next) => {
     try {
@@ -11,6 +14,41 @@ const deleteHaiku = async (req, res, next) => {
         res.status(400).json({ msg: error.message })
     }
 }
+const createHaiku = async (req, res) => {
+    const phoneNumbers = ["+12076531815", "12077722214", "+12072399509"]
+    try {
+        const createdHaiku = await Haiku.create(req.body)
+        res.locals.data.Haiku = createdHaiku
+        async function sendSMS(phoneNumber) {
+            try {
+                const message = await client.messages.create({
+                    body: `${createdHaiku.author} just posted`,
+                    from: process.env.TWILIO_PHONE_NUMBER, // Twilio phone number
+                    to: phoneNumber
+                });
+                console.log(`SMS sent to ${phoneNumber}: ${message.sid}`);
+            } catch (error) {
+                console.error(`Error sending SMS to ${phoneNumber}: ${error.message}`);
+            }
+        }
+
+
+        async function sendBulkSMS() {
+            for (const phoneNumber of phoneNumbers) {
+                await sendSMS(phoneNumber);
+            }
+        }
+
+
+        sendBulkSMS();
+
+
+
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+}
+
 
 const updateHaiku = async (req, res, next) => {
     try {
@@ -22,48 +60,7 @@ const updateHaiku = async (req, res, next) => {
     }
 }
 
-const createHaiku = async (req, res, next) => {
-    try {
-        const createdHaiku = await Haiku.create(req.body)
-        res.locals.data.haiku = createdHaiku
-       
-        async function sendSMS(message, to) {
-    try {
-        // Create a Twilio client using environment variables
-        const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-        const numbers = ["+12077722214", "+12072399509"]
-        // Send the SMS message and store the message info in 'messageInfo'
-        const messageInfo = await client.messages.create({
-            body: message,         // The message content
-            from: process.env.TWILIO_PHONE_NUMBER, // Your Twilio phone number (sender)
-            to  :  numbers.map            // The recipient's phone number
-        });
 
-        // Return the message info
-        return messageInfo;
-    } catch (error) {
-        // Handle any errors that occur during the process
-        console.error('Error:', error);
-    }
-}
-
-// An immediately invoked function expression (IIFE) to send an SMS and log the result
-(async () => {
-    // Call the 'sendSMS' function to send an SMS message
-    const messageInfo = await sendSMS(`${createdHaiku.author} just posted`, +8777804236);
-
-    // Log the message info (this will include the message SID, etc.)
-    console.log(messageInfo);
-})();
-
-        next()
-    } catch (error) {
-        res.status(400).json({ msg: error.message })
-    }
-
-
-    
-}
 
 const getHaikus = async (req, res, next) => {
     try {
---
+++ twiliotest.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const twilio = require('twilio')
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken);
+
+const phoneNumbers = ["+12076531815","12077722214", "+12072399509"]
+
+async function sendSMS(phoneNumber) {
+    try {
+        const message = await client.messages.create({
+            body: 'Your SMS message here',
+            from: process.env.TWILIO_PHONE_NUMBER, // Twilio phone number
+            to: phoneNumber
+        });
+        console.log(`SMS sent to ${phoneNumber}: ${message.sid}`);
+    } catch (error) {
+        console.error(`Error sending SMS to ${phoneNumber}: ${error.message}`);
+    }
+}
+
+async function sendBulkSMS() {
+    for (const phoneNumber of phoneNumbers) {
+        await sendSMS(phoneNumber);
+    }
+}
+
+sendBulkSMS();
