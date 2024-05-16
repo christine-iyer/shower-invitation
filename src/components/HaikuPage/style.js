ecb863666e874ad04b11ab14162ca7212e6921ff
chrisIyer <hellohotbox420@gmail.com>
2024-05-16 19:10:46 -0400

    tiny. want to find a fancy button.

---
+++ src/components/HaikuPage/style.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './style.css';
+
+export default function Buttons({ filterItem, setItem, menuItems,haikus }) {
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+        {menuItems?.map((Val, _id) => {
+          return (
+            <button
+              className="btn-light text-green p-1 px-1 mx-.125 btn "
+              onClick={() => filterItem(Val)}
+              key={_id}
+            >
+              {Val}
+            </button>
+          );
+        })}
+        <button
+          className="btn-dark text-white p-1 px-1 mx-.125  btn"
+          
+          onClick={() => setItem(haikus)}
+        >
+          All
+        </button>
+
+
+      </div>
+    </>
+  )
+}
+
+
+
