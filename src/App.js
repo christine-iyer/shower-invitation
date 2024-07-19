5aa18217c96e6fef0f010c29389a640628ce7064
chrisIyer <hellohotbox420@gmail.com>
2024-07-19 12:16:25 -0400

    2 servers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Bar from './components/Bar/Bar'
 import BlahgPage from './pages/BlahgPage/BlahgPage'
 import HaikuPage from "./pages/HaikuPage/HaikuPage";
-import Roth from "./pages/RothPage/RothPage";
+import FlipCard from "./pages/flipPage/FlipCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './App.css';
@@ -15,7 +15,7 @@ export default function App() {
             <BrowserRouter>
                 <Bar />
                 <Routes>
-                    <Route path='/' element={<Roth />} />
+                    <Route path='/' element={<FlipCard />} />
                     <Route path='/haikus' element={<HaikuPage />} />
                     <Route path='/blahg' element={<BlahgPage />} />
                 </Routes>
