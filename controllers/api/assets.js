a8ba7b2937ee646e8b83164e5e0875e578a88a4c
Christine Iyer <93498660+christine-iyer@users.noreply.github.com>
2024-12-02 19:18:25 -0500

    Update assets.js

--- controllers/api/assets.js
+++ controllers/api/assets.js
@@ -5,12 +5,20 @@ const Asset = require('../../models/asset')
 const axios = require("axios");
 
 const hardcodedData = [
+  { symbol: "AMD", purchasePrice: 152.31, shares: 1, principalDate: '10/23/2024' },
+  { symbol: "AMD", purchasePrice: 242.54, shares: 4, principalDate: '10/11/2024' },
+  { symbol: "AI", purchasePrice: 37.50, shares: 16, principalDate: '11/29/2024' },
+  { symbol: "AI", purchasePrice: 22.99, shares: 8, principalDate: '10/03/2024' },
+    { symbol: "AI", purchasePrice: 30.21, shares: 25, principalDate: '10/03/2024' },
+  
+ 
   { symbol: "GOOGL", purchasePrice: 143.49, shares: 100, principalDate: '12/22/2021' },
   { symbol: "TSLA", purchasePrice: 242.54, shares: 45, principalDate: '11/27/2020' },
   { symbol: "AMZN", purchasePrice: 160.00, shares: 80, principalDate: '11/27/2020' },
   { symbol: "BA", purchasePrice: 188.59, shares: 25, principalDate: '12/14/2022' },
   { symbol: "COIN", purchasePrice: 257.31, shares: 50, principalDate: '12/28/2021' },
   { symbol: "AAPL", purchasePrice: 114.56, shares: 200, principalDate: ' 11/23/2020' },
+  { symbol: "AAPL", purchasePrice: 209.20, shares: 10, principalDate: ' 08/05/2024' },
   { symbol: "CVS", purchasePrice: 89.83, shares: 25, principalDate: '1/11/2023' },
   { symbol: "GS", purchasePrice: 342.94, shares: 10, principalDate: '1/24/2022' },
   { symbol: "MS", purchasePrice: 98.39, shares: 25, principalDate: '12/22/2021' },
