d3b53cb1d7177459abba2b514706f9939aca48d9
chrisIyer <hellohotbox420@gmail.com>
2025-07-25 13:58:56 -0400

    added routes

--- routes/api/review.js
+++ routes/api/review.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const reviewController = require('../../controllers/api/review.js')
 
-// Basic CRUD routes
 router.post('/', reviewController.writeReview)
 router.put('/:id', reviewController.editReview)
 router.delete('/:id', reviewController.deleteReview)
@@ -11,6 +10,8 @@ router.get('/', reviewController.listReviews)
 // NEW: Like and comment routes
 router.put('/:id/like', reviewController.likeReview)              // Like a review
 router.post('/:id/comment', reviewController.addComment)          // Add comment to review
-router.put('/:id/comment/:commentId/like', reviewController.likeComment) // Like a comment
+router.put('/:id/comment/:commentId', reviewController.editComment)    // Edit a comment
+router.delete('/:id/comment/:commentId', reviewController.deleteComment) // Delete a comment
+router.post('/:id/comment/:commentId/like', reviewController.likeComment) // Like a comment (changed from PUT to POST)
 
 module.exports = router
